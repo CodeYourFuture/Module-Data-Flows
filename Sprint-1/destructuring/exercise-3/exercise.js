c5359de4839329cd78c11289ec0bb3a77b6c7f18
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-06 00:32:44 +0000

    Sprint-1 - destructiring - exercise 3 - exercise

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,18 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+function receipt(order){
+  let total = 0;
+  console.log("QTY        ITEM               TOTAL")
+  for(let index in order){
+    let {itemName, quantity, unitPricePence} = order[index];
+  
+    console.log(quantity + "          " + itemName + " ".repeat(19 - itemName.length) + unitPricePence);
+    total += unitPricePence;
+   
+
+  }
+  console.log("Total:  " + total);
+}
+
+receipt(order);
