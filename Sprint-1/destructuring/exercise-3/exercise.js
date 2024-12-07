e6b65779eea74417c1d963fb513e44704ad56100
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-07 13:11:41 +0000

    update

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -1,18 +1,29 @@
 let order = [
   { itemName: "Hot cakes", quantity: 1, unitPricePence: 232 },
+  { itemName: "Falafel with salad", quantity: 1, unitPricePence: 224},
   { itemName: "Apple Pie", quantity: 2, unitPricePence: 139 },
   { itemName: "Egg McMuffin", quantity: 1, unitPricePence: 280 },
   { itemName: "Sausage McMuffin", quantity: 1, unitPricePence: 300 },
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+let sortedOrder = order.sort((a, b) => a.itemName.length - b.itemName.length);
+let longestItemName = sortedOrder.length - 1;
+console.log(sortedOrder[5]);
 function receipt(order){
   let total = 0;
-  console.log("QTY        ITEM               TOTAL")
+  console.log("QTY        ITEM                 TOTAL")
   for(let index in order){
     let {itemName, quantity, unitPricePence} = order[index];
-  
-    console.log(quantity + "          " + itemName + " ".repeat(19 - itemName.length) + unitPricePence);
+    if((sortedOrder[longestItemName].itemName.length - itemName.length) == 0){
+      console.log(quantity + "          " + itemName + "   " + unitPricePence);
+
+    }
+    else{
+      console.log(quantity + "          " + itemName + " ".repeat(sortedOrder[longestItemName].itemName.length - itemName.length) + "   " + unitPricePence);
+
+    }
     total += unitPricePence;
    
 
