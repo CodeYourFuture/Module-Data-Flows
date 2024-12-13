0d34709a5bca8aa8b2d741ec37e4ec822d884c20
nadika <nadikazavodovska@gmail.com>
2024-12-13 16:56:02 +0000

    Manipulate with data object (response), to retrive image

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -12,11 +12,17 @@ function fetchApiData() {
         } else{
         // if response is fine, return response in JSON and convert it to JS object.
             return response.json();
-        }
-
+        }     
     })
-
-    
+    // after the first then succecsfully passed, we can manage with JS object data.
+    .then(data => {
+        // create img tag.
+        const imgHumor = document.createElement("img");
+        // assign value from data object to src in img tag.
+        imgHumor.src = data.img;
+        // Add image as a child to div with class container on html.
+        container.appendChild(imgHumor);
+    })    
 }
 
 console.log(fetchApiData());
