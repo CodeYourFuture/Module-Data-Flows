42fcaa2abe30b0c5e3769854cd88cce391b391a7
sabafarjamfard <farjamfardsaba@yahoo.com>
2024-12-13 08:57:58 +0000

    completing (exercise-3) exercise.js

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,16 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY".padEnd(5) + "ITEM".padEnd(20) + "TOTAL");
+let totalCost = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const total = (quantity * unitPricePence) / 100; // Convert pence to dollars
+  console.log(
+    `${quantity.toString().padEnd(5)}${itemName.padEnd(20)}${total.toFixed(2)}`
+  );
+  totalCost += total;
+});
+
+console.log("\nTotal: " + totalCost.toFixed(2));
