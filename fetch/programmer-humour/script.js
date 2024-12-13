daed6cf872f23b22eaeed0062de8d719f1bd9cc8
nadika <nadikazavodovska@gmail.com>
2024-12-13 16:34:30 +0000

    Assign api url to variable, get container by id

---
+++ fetch/programmer-humour/script.js
@@ -0,0 +1,11 @@
+function fetchApiData() {
+    // assign API url to a variable.
+    const url = "https://xkcd.now.sh/?comic=latest";
+    // get container by id
+    const container = document.getElementById("container");
+    
+    
+    
+}
+
+console.log(fetchApiData());
