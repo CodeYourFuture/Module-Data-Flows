6ce3e7deeb5bc4a36c7d477043a4dd93908e5c2c
RahwaZeslusHaile <rahwazeslase@gmail.com>
2024-12-14 15:56:10 +0000

    destructuring an object in an array

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -2,22 +2,22 @@ let hogwarts = [
   {
     firstName: "Harry",
     lastName: "Potter",
-    house: "Gryffindor",
-    pet: "Owl",
+    house: "Gryffindor",//1
+    pet: "Owl",//1
     occupation: "Student",
   },
   {
     firstName: "Ron",
     lastName: "Weasley",
-    house: "Gryffindor",
-    pet: "Scabbers",
+    house: "Gryffindor",//2
+    pet: "Scabbers",//2
     occupation: "Student",
   },
   {
     firstName: "Hermione",
     lastName: "Granger",
-    house: "Gryffindor",
-    pet: "Cat",
+    house: "Gryffindor",//3
+    pet: "Cat",//3
     occupation: "Student",
   },
   {
@@ -58,15 +58,39 @@ let hogwarts = [
   {
     firstName: "Minerva",
     lastName: "McGonagall",
-    house: "Gryffindor",
+    house: "Gryffindor",//4
     pet: null,
     occupation: "Teacher",
   },
   {
     firstName: "Albus",
     lastName: "Dumbledore",
-    house: "Gryffindor",
-    pet: "Phoenix",
+    house: "Gryffindor",//5
+    pet: "Phoenix",//4
     occupation: "Teacher",
   },
 ];
+//will take the `hogwarts` array as input and display the names of the people who belong to the Gryffindor house.
+// Harry Potter
+// Ron Weasley
+// Hermione Granger
+// Minerva McGonagall
+// Albus Dumbledore
+function belongToGryffindor(){
+  
+   for (const { firstName, lastName, house } of hogwarts) {
+      if(house === "Gryffindor"){
+         console.log(`${firstName} ${lastName}`);
+
+  }
+   }
+}belongToGryffindor();
+//- In `exercise.js` write a program that will take the `hogwarts` array as input and display the names of teachers who have pets.
+
+function teachersWithPets(){
+ for(const{ firstName, lastName, occupation,pet} of hogwarts){
+  if(occupation==="Teacher" && pet !== null)
+    console.log(`${firstName} ${lastName}`);
+}
+}
+teachersWithPets()
