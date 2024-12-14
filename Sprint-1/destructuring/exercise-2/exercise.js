21717898128346411bfce49e0e570083fb7c77c7
bestunmoh <shazdsp@gmail.com>
2024-12-14 15:27:19 +0000

    Module-Data-flows-exercise-2

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -3,21 +3,21 @@ let hogwarts = [
     firstName: "Harry",
     lastName: "Potter",
     house: "Gryffindor",
-    pet: "Owl",
+    pet: "Owl",//1
     occupation: "Student",
   },
   {
     firstName: "Ron",
     lastName: "Weasley",
     house: "Gryffindor",
-    pet: "Scabbers",
+    pet: "Scabbers",//2
     occupation: "Student",
   },
   {
     firstName: "Hermione",
     lastName: "Granger",
     house: "Gryffindor",
-    pet: "Cat",
+    pet: "Cat",//3
     occupation: "Student",
   },
   {
@@ -66,7 +66,31 @@ let hogwarts = [
     firstName: "Albus",
     lastName: "Dumbledore",
     house: "Gryffindor",
-    pet: "Phoenix",
+    pet: "Phoenix",//4
     occupation: "Teacher",
   },
 ];
+function GryffindorHouse (hogwarts){
+  for(let index = 0; index < hogwarts.length; index++){
+    let {firstName, lastName, house} = hogwarts[index];
+    if(house == "Gryffindor"){
+      console.log(firstName + " " + lastName);
+  }
+  }
+}
+
+//- In `exercise.js` write a program that will take the `hogwarts` array as input and display the names of teachers who have pets.
+function pets ( hogwarts){
+  for(let index = 0; index < hogwarts.length; index++){
+    let {pet, occupation ,firstName ,lastName} = hogwarts[index];
+    if(occupation === "teacher"){
+      if(pet !== null)
+      {
+        console.log(`${firstName} ${lastName}`)
+      }
+    }
+  }
+}
+
+GryffindorHouse(hogwarts);
+pets(hogwarts);
