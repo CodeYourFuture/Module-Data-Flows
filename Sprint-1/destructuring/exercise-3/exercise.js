30f8b627e9ee8cfd38adc9e03375e39fb21ce715
bestunmoh <shazdsp@gmail.com>
2024-12-14 15:43:02 +0000

    /Sprint-1/destructuring/exercise-3/exercise.js

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+// Header
+console.log("QTY     ITEM                TOTAL");
+
+let totalCostPence = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const itemTotalPence = quantity * unitPricePence;
+  totalCostPence += itemTotalPence;
+
+  // Format item output
+  const formattedItemTotal = (itemTotalPence / 100).toFixed(2);
+  console.log(
+    `${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${formattedItemTotal}`
+  );
+});
+
+// Total cost
+console.log(`\nTotal: ${(totalCostPence / 100).toFixed(2)}`);
