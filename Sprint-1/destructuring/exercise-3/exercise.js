9aaae931f605bdd82b004717fad60c2b8c1680f2
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-12-14 15:50:22 +0000

    Exercise - 3
    complete

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,14 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+let totalCost = 0
+
+console.log("QTY      ITEM              TOTAL")
+
+order.forEach(({itemName, quantity, unitPricePence}) => {
+  const totalItemCost = (unitPricePence * quantity) / 100
+    totalCost += totalItemCost
+   
+    console.log(`${quantity}       ${itemName.padEnd(18)} ${totalItemCost.toFixed(2)}`)
+})
