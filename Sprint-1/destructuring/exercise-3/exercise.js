c37f604521dbb7c474d92c06c785ed0e4c2bedce
Ameneh-KM <ameneh.keshavarz@gmail.com>
2024-12-17 09:12:45 +0000

    Sprint-1-destructuring

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,15 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+console.log(`QTY     ITEM                TOTAL`);
+let Total = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const Cost = (quantity * unitPricePence) / 100;
+  Total += Cost;
+  console.log(
+    `${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${Cost.toFixed(2)}`
+  );
+});
+
+console.log(`\nTotal: ${Total.toFixed(2)}`);
