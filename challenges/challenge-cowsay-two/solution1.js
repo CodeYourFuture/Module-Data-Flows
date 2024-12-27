1e442e97f1ba831d3ae1fa335bed7f3de16eb06f
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-27 22:03:00 +0000

    s-1 maling the function work

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -8,18 +8,45 @@
 
 // how will you accept arguments?
 
+const readLine = require("node:readline");
+const reader = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.question("Write a word please: ", word => {
+    
+})
+
 // 2. Make supplies for our speech bubble
 
 let topLine = '_';
 let bottomLine = '-';
-let saying = '';
+let sayingWord = '';
 
+let cow = `
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+    `;
 // 3. Make a cow that takes a string
 
 function cowsay(saying) {
 // how will you make the speech bubble contain the text?
+    for(let i =0; i < saying.length; i++){
+        topLine += "_";
+        bottomLine += "-";
+    }
+    sayingWord = `< ${saying} >`;
 
 // where will the cow picture go?
+
+    console.log(` ${topLine}
+${sayingWord}
+ ${bottomLine}
+${cow}`)
 
 // how will you account for the parameter being empty?
 
@@ -28,3 +55,4 @@ function cowsay(saying) {
 //4. Pipe argument into cowsay function and return a cow
 
 // how will you log this to the console?
+cowsay("hi");
