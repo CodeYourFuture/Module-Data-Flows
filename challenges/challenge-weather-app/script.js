5c82ead1b2aae6daefa38059d0dff4292f6066ba
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-12-29 14:27:05 +0000

    found api keys

---
+++ challenges/challenge-weather-app/script.js
@@ -0,0 +1,3 @@
+const myWeatherApi = "http://api.openweathermap.org/data/2.5/weather?q=london&appid=6e49ae8965cb6f613d911543030f6a21";
+const myUnsplashApi = "https://api.unsplash.com/search/photos?query=snow&client_id=9g7Ek1p5R0VH2oqrNqOmgA9ndGUzh_2YIfYqOhazMB8";
+
