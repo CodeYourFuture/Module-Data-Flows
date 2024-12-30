231b374bb389ee4d9170114489a04abf7f786bc9
mgphone <pmn.blazer@gmail.com>
2024-12-30 03:23:20 +0000

    destructuring exercise one

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,10 +6,18 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself(person) {
+  const {name,age,favouriteFood}=person
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
 introduceYourself(personOne);
+
+// # Exercise
+
+// - What is the syntax to destructure the object `personOne` in exercise.js?
+// - Update the parameter of the function `introduceYourself` to use destructuring on the object that gets passed in.
+
+// PersonOne is calling the Object as paraameter of the function introduceYourself
