5181dcedc27c14a4c24102e7470dbb0be287afc1
mgphone <pmn.blazer@gmail.com>
2024-12-30 05:15:27 +0000

    desctructing array

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,15 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+const newOrder=order.map(({quantity,itemName,unitPricePence})=>({quantity,itemName,unitPricePence:twoDigitNumber(unitPricePence*quantity)}))
+
+function twoDigitNumber(value){
+  return (value/100).toFixed(2)
+}
+function totalResult(newOrder){
+  return newOrder.reduce((a,{unitPricePence})=>a+Number(unitPricePence),0).toFixed(2)
+
+}
+console.log(newOrder)
+console.log("Total: "+totalResult(newOrder))
