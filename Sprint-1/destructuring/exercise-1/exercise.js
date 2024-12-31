04bae5abefc676527ae512b7f29ddf365224550c
David Liandja <72330143+apachitech@users.noreply.github.com>
2024-12-31 11:39:20 +0200

    Applying space for proper formatting
    The suggestion to apply proper formatting was implemented by adding spaces between variable names in the destructured parameters. This aligns with readability and best practices for clean code.

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,7 +6,7 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself({name,age,favouriteFood}) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
@@ -17,8 +17,8 @@ introduceYourself(personOne);
 /*
 or: 
 
-function introduceYourself({name,age,favouriteFood}) {
-  const {name,age,favouriteFood} = personOne;
+function introduceYourself({ name, age, favouriteFood}) {
+  const { name, age, favouriteFood } = personOne;
 
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
