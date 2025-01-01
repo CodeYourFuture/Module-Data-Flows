fd5a9e4a2b05f41177e51242959604ed2a1132bf
Serna Malala <sm.sernamalala@gmail.com>
2025-01-01 19:46:39 +0200

    I created a takeOutOrder function to format the information in the array and print out the given order according to what was given in the readme file'

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,23 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+
+function takeoutOrder(arrayInput) {
+  let total = 0;
+  console.log(`${"QTY".padEnd(8)}${"ITEM".padEnd(20)}${"TOTAL"}`);
+
+  for (const { itemName, quantity, unitPricePence } of arrayInput) {
+    let singularTotalPrice = ((unitPricePence / 100) * quantity);
+    //Can use padEnd to add spaces at the end of each variable
+    //Total has to be adjusted according to pounds
+    console.log(`${quantity.toString().padEnd(8)}${itemName.toString().padEnd(20)}${singularTotalPrice.toFixed(2)}`);
+    total += singularTotalPrice;
+  }
+  console.log(`\nTotal price: ${total.toFixed(2)}`)
+
+
+}
+
+takeoutOrder(order);
