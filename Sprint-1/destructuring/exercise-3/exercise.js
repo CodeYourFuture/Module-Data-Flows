321b3d9306216654c4e2e5dfc438db8d147d014e
gai93003 <deng93003@gmail.com>
2025-01-01 11:36:35 +0000

    I finished this exercise of the price list

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,16 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+let totalPrice = 0;
+
+console.log('QTY     ITEM                TOTAL')
+
+for (const {itemName, quantity, unitPricePence} of order) {
+  price = ((quantity * unitPricePence) / 100);
+  totalPrice += price;
+
+  console.log(`${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${price.toFixed(2)}`);
+}
+
+console.log(`Total: ${totalPrice.toFixed(2)}`)
