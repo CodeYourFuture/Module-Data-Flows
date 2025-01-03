995bbf72e4c2d304615e28859759395101f34ed8
Emmanuel <esiziba71@gmail.com>
2025-01-03 15:10:36 +0200

    takout order function

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -1,4 +1,4 @@
-let order = [
+const order = [
   { itemName: "Hot cakes", quantity: 1, unitPricePence: 232 },
   { itemName: "Apple Pie", quantity: 2, unitPricePence: 139 },
   { itemName: "Egg McMuffin", quantity: 1, unitPricePence: 280 },
@@ -6,3 +6,15 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY     ITEM                TOTAL"); 
+
+let totalPence = 0; 
+for (const item of order) {
+  const { itemName, quantity, unitPricePence } = item;
+  const itemTotalPence = quantity * unitPricePence; 
+  totalPence += itemTotalPence;
+  console.log(`${quantity}       ${itemName.padEnd(20)} ${itemTotalPence / 100}`); 
+}
+
+console.log("\nTotal:", totalPence / 100);
