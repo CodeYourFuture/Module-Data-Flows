4797cfa03c322126577230990b36ee668919b3ad
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-01-05 22:16:07 +0000

    passing tests - get average - completed

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -1,7 +1,16 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
-
-function average(numbers) {}
-
+let numbers = [4, "-", 8, 11, "hello", "57", 0, 2, "hi", 22];
+function average(numbers) {
+    let numCount = 0;
+    for (let item in numbers){
+        if (typeof(numbers[item]) == "number"){
+            numCount += 1;
+        }
+    }
+    console.log(numCount);
+    return numCount;
+}
+average(numbers);
 module.exports = average;
