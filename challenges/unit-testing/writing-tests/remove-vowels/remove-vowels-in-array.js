db55026d611ada1494f1bbc676b37a9664be3a82
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-01-05 22:51:19 +0000

    unit testing - writing tests - remove vowels

--- challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.js
+++ challenges/unit-testing/writing-tests/remove-vowels/remove-vowels-in-array.js
@@ -1,13 +1,13 @@
 let removeVowels = require("./remove-vowels");
-
+let arr = ["Irina", "Etza", "Daniel"];
 function removeVowelsFromWords(words) {
   let result = words.map(function (word) {
     return removeVowels(word);
   });
-
+  console.log(result);
   return result;
 }
-
+removeVowelsFromWords(arr);
 module.exports = removeVowelsFromWords;
 
 /*
