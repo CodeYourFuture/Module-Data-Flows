19de06a034824bd61ca9f428cd654619d8773939
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-01-11 23:17:45 +0000

    changed let to const

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,7 +3,7 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
-let {name, age, favouriteFood} = personOne;
+const {name, age, favouriteFood} = personOne;
 // Update the parameter to this function to make it work.
 // Don't change anything else.
 function introduceYourself({name, age, favouriteFood}) {
