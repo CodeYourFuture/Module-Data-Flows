895f5a1474a68b86222e3736bbc6950bb3838866
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-12 01:47:04 +0000

    Fix bugs in book adding, deleting, and read status functionality

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
 
-window.addEventListener("load", function (e) {
+window.addEventListener("load", function () {
   populateStorage();
   render();
 });
@@ -37,8 +37,10 @@ function submit() {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    //Correctly pass author.value 
+    let book = new Book(title.value, author.value, pages.value, check.checked);
+    //Replace library.push(book) with myLibrary.push(book)
+    myLibrary.push(book);
     render();
   }
 }
@@ -54,7 +56,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
@@ -76,7 +78,8 @@ function render() {
     changeBut.className = "btn btn-success";
     wasReadCell.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
+    //Fix the logic for the "Read" statues true for "Yes", false for No
+    if (myLibrary[i].check == true) {
       readStatus = "Yes";
     } else {
       readStatus = "No";
@@ -89,12 +92,14 @@ function render() {
     });
 
     //add delete button to every row and render again
+    // correct delButton in rows
     let delButton = document.createElement("button");
-    delBut.id = i + 5;
-    deleteCell.appendChild(delBut);
-    delBut.className = "btn btn-warning";
-    delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
+    delButton.id = i + 5;
+    deleteCell.appendChild(delButton);
+    delButton.className = "btn btn-warning";
+    delButton.innerHTML = "Delete";
+    //Fix the event listener: "click" instead of "clicks".
+    delButton.addEventListener("click", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
