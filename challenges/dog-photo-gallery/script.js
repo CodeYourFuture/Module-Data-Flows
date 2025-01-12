29afe6a7049d27cbcf0c2644e2d5a33bb5a55558
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-01-12 00:23:37 +0000

    update

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,20 +1,26 @@
 document.getElementById('btn-click').addEventListener('click', () => {
-    fetch("https://dog.ceo/api/breeds/image/random").then(Response => Response.json())
+    fetch("https://dog.ceo/api/breeds/image/random").then(response => response.json())
     .then(data =>{
         console.log(data)
-        let photoLink = data.message;
+        const photoLink = data.message;
 
         let ulElement = document.querySelector("#gallery");
         let liNew = document.createElement("li");
 
-        liNew.innerHTML = `<img src="${photoLink}" class="li-img" alt="random dog">`;
+        // Create an img element
+        let imgNew = document.createElement("img");
+        imgNew.src = photoLink;
+        imgNew.alt = "random dog";
+        imgNew.className = "li-img";
+
+        liNew.appendChild(imgNew);
         ulElement.appendChild(liNew);
     })
     .catch(error => {
         console.error("Error fetching dog image:", error);
         alert("Oops! Something went wrong. Please try again.");
     });
-})
+});
 
 
 
