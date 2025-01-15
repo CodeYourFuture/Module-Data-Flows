e4e692cb174448c84d0e85400b3e1a928455b5f6
rawrnoodles <matthew.lt.law@hotmail.com>
2025-01-15 21:46:56 +0000

    get-average.js - created get-average program

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,13 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let numericValues = numbers.filter(item => typeof item === "number");
+  if (numericValues.length === 0) {
+    return 0;
+  }
+  let sum = numericValues.reduce((acc, num) => acc + num, 0);
+  return sum / numericValues.length;
+}
 
 module.exports = average;
