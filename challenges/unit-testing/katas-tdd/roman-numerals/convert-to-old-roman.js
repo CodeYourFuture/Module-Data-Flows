0a68597e4545739a8a12e037009eae263decc57f
rawrnoodles <matthew.lt.law@hotmail.com>
2025-01-15 21:38:05 +0000

    convert-to-new-roman.js and convert-to-old-roman.js - created programs with jest tests

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,22 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 500, numeral: 'D' },
+        { value: 100, numeral: 'C' },
+        { value: 50, numeral: 'L' },
+        { value: 10, numeral: 'X' },
+        { value: 5, numeral: 'V' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    let result = '';
+    for (const { value, numeral } of romanNumerals) {
+        while (n >= value) {
+            result += numeral;
+            n -= value;
+        }
+    }
+    return result;
+}
 
 module.exports = convertToOldRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -2,6 +2,12 @@ let convertToOldRoman = require("./convert-to-old-roman");
 
 test("returns I if passed 1 as an argument", function () {
   // Arrange
+  const input = 1;
+  const expectedOutput = "I";
+
   // Act
+  const result = convertToOldRoman(input);
+
   // Assert
+  expect(result).toBe(expectedOutput);
 });
