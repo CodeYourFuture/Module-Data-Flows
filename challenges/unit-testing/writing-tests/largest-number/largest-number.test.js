88cb467a60df5db88a749b1e8481a889595aaad4
rawrnoodles <matthew.lt.law@hotmail.com>
2025-01-15 21:54:01 +0000

    largest-number.test.js - created jest test

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -2,12 +2,13 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  const input = [3, 21, 88, 4, 36];
+  const expected = 88;
+  
   // Act
+  const result = getLargestNumber(input);
+
   // Assert
+  expect(result).toBe(expected);
+  expect(input).toEqual([3, 21, 88, 4, 36]); // Ensures the original array hasn't changed
 });
-
-// example
-// input: [3, 21, 88, 4, 36];
-// expected: 88;
-
-// also test that the original array hasn't changed
