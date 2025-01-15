9284c6b7b674e2ae459f62fa4c43957060364f88
rawrnoodles <matthew.lt.law@hotmail.com>
2025-01-15 21:41:44 +0000

    car-sales.js - created car sales program

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,8 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  return carsSold.reduce((acc, car) => {
+    acc[car.make] = (acc[car.make] || 0) + car.price;
+    return acc;
+  }, {});
+}
 
 module.exports = sales;
