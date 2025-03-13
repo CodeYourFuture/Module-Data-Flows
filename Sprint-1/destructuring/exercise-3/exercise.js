05effb7ca4b4305b8559798166852b72fc09d84a
Droid-An <andrmail6@gmail.com>
2025-03-13 19:03:39 +0000

    done 3 exercise

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -1,36 +1,50 @@
 let order = [
   { itemName: "Hot cakes", quantity: 1, unitPricePence: 232 },
   { itemName: "Apple Pie", quantity: 2, unitPricePence: 139 },
-  { itemName: "Egg McMuffin", quantity: 1, unitPricePence: 28000 },
+  { itemName: "Egg McMuffin", quantity: 1, unitPricePence: 280 },
   { itemName: "Sausage McMuffin", quantity: 1, unitPricePence: 300 },
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
 
 const nameLength = findMaxItemNameLength(order, 'itemName')
-const totalLength = findMaxItemNameLength(order, 'unitPricePence')
 
-console.log({nameLength, totalLength})
+let totalInPence = 0
+for (item of order) {
+  totalInPence += item.unitPricePence * item.quantity
+}
+
+console.log(`QTY\t${"ITEM".padEnd(nameLength," ")}   TOTAL`)
+order.map(printIndividualItem)
+console.log("")
+console.log(`Total: ${penceToPounds(String(totalInPence))}`);
 
 function printIndividualItem({itemName, quantity, unitPricePence}) {
-  const priceInPence = String(unitPricePence * quantity)
-  const priceInPounds = priceInPence.replace(/^(.{1})/, '$1.');
+  const itemPriceInPence = String(unitPricePence * quantity)
+  const itemPriceInPounds = penceToPounds(itemPriceInPence);
  
-  console.log(`${quantity}\t${itemName.padEnd(nameLength," ")}   ${priceInPounds}`)
+  console.log(`${quantity}\t${itemName.padEnd(nameLength," ")}   ${itemPriceInPounds}`)
   return quantity
 }
 
-console.log(`QTY\tITEM   TOTAL`)
-order.map(printIndividualItem)
-console.log("")
-console.log("Toatal")
-
-
-
-function findMaxItemNameLength(order, key) {
-  let ggg = order.map((item) => String(item[key]).length)
-  return Math.max(...ggg)
+function penceToPounds (penceString) {
+    const paddedPenceNumberString = penceString.padStart(3, "0");
+    const pounds = paddedPenceNumberString.substring(
+      0,
+      paddedPenceNumberString.length - 2
+    );
+    
+    const pence = paddedPenceNumberString
+      .substring(paddedPenceNumberString.length - 2)
+      .padEnd(2, "0");
+    
+    return (`${pounds}.${pence}`);
 }
-console.log(findMaxItemNameLength(order))
 
-// console.log(Math.max(...order.map(calculateItemNameLength)))
+function findMaxItemNameLength(order) {
+  let ItemNameLengthList = order.map(({itemName}) => itemName.length )
+  return Math.max(...ItemNameLengthList)
+}
+
+// this issue requires more than 1 hour
+// what would be better to use: regexp or pence to pound conversion
