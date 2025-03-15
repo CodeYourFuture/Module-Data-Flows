099cc352e1be176721e2710f7934d758527d9a23
eyuell21 <abrahameyuel4@gmail.com>
2025-03-15 01:01:20 +0000

    Written a program that will print out the receipt for the order.

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,46 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function orderSummary(params) {
+  let totalCost = 0 
+
+  console.log(
+    "QTY".padEnd(8) +   // aligning each header with the rest of the list
+    "ITEM".padEnd(16) + 
+    "TOTAL".padStart(12)
+  );
+  order.forEach(({quantity, itemName, unitPricePence})=> {  // calculating the total quantity and displaying each item
+
+    let total = (quantity * unitPricePence) / 100
+    
+    console.log(
+      `${quantity.toString().padEnd(8)}` +  // aligning each Item with the header
+      `${itemName.padEnd(16)}` + 
+      `${(total.toFixed(2)).padStart(12)}`)
+    return totalCost += total
+  })
+  console.log(`Total: ${totalCost.toFixed(2)}`)
+}
+
+
+
+
+
+
+
+// const [
+//   {itemName:itemName1, quantity:quantity1, unitPricePence:unitPricePence1},
+//   {itemName:itemName2, quantity:quantity2, unitPricePence:unitPricePence2},
+//   {itemName:itemName3, quantity:quantity3, unitPricePence:unitPricePence3},
+//   {itemName:itemName4, quantity:quantity4, unitPricePence:unitPricePence4},
+//   {itemName:itemName5, quantity:quantity5, unitPricePence:unitPricePence5},
+//   {itemName:itemName6, quantity:quantity6, unitPricePence:unitPricePence6}
+
+// ] = order
+
+
+
+function takeoutSummary(){
+  
+}
