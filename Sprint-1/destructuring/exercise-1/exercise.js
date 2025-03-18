3491fb38a7ebc0bf91768d4990317a69c2ffe8e7
sheetalkharab <sheetalrana89@gmail.com>
2025-03-18 17:17:18 +0000

    update parameter of function in exercise1

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,10 +3,13 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+// What is the syntax to destructure the object `personOne` in exercise.js?
+// const {property1, property2, property3} = personOne;
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+//function introduceYourself(___________________________) {
+function introduceYourself({name, age, favouriteFood}){
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
