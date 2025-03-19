3bbbb69424f5a816c2185512f4663ec33923cc5a
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-03-19 21:30:19 +0000

    commit for exercise 1

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,10 +3,12 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+let {name, age, favouriteFood} = personOne;
+console.log(`${name} and ${age}`);
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({name,age,favouriteFood}) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
