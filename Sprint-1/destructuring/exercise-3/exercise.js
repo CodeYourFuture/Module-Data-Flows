1468b7bbc8415a19b74bd028e2edf8d76af84c7d
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-04-07 17:17:15 +0100

    Exercise 3 - Destructuring

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,23 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function receipt() {
+  console.log(`QTY     ITEM                 TOTAL`);
+  let total = 0;
+
+  for (const { itemName, quantity, unitPricePence } of order) {
+    const unitPricePounds = unitPricePence / 100;
+    const totalOneItem = quantity * unitPricePounds;
+    total += totalOneItem;
+
+    const itemNameFormatted = itemName.padEnd(20);
+    const totalFormatted = totalOneItem.toFixed(2).padStart(6);
+
+    console.log(`${quantity}       ${itemNameFormatted}${totalFormatted}`);
+  }
+
+  console.log(`\nTotal: ${total.toFixed(2)}`);
+}
+
+receipt(order);
