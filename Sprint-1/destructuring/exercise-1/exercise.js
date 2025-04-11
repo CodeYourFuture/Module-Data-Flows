2ce1435e3d69f552ee1cd1f41fe000e3f2b34e3c
sripathman <sripathmann@gmail.com>
2025-04-11 07:55:31 +0100

    Trying after checking out the 'b972e3e' state of destructuring

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -1,14 +1,14 @@
 const personOne = {
   name: "Popeye",
   age: 34,
-  favoriteFood: "Spinach",
+  favouriteFood: "Spinach",
 };
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself( name, age, favoriteFood) {
+function introduceYourself(___________________________) {
   console.log(
-    `Hello, my name is ${name}. I am ${age} years old and my favorite food is ${favoriteFood}.`
+    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,20 +6,3 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
-
-console.log("QTY     ITEM                    TOTAL");
-
-let totalCostPence = 0;
-
-for (const item of order) {
-  const { itemName, quantity, unitPricePence } = item;
-  const totalItemCostPence = quantity * unitPricePence;
-  totalCostPence += totalItemCostPence;
-  const totalItemCostPounds = (totalItemCostPence / 100).toFixed(2);
-  console.log(
-    `${quantity.toString().padEnd(6)} ${itemName.padEnd(24)} ${totalItemCostPounds}`
-  );
-}
-
-const totalCostPounds = (totalCostPence / 100).toFixed(2);
-console.log("\nTotal: " + totalCostPounds);
