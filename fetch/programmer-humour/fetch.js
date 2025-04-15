723dbfddafdb7f9e6f73b594b7720ddd833b9d4c
Droid-An <andrmail6@gmail.com>
2025-04-15 14:20:46 +0100

    added styles and errors catch

--- fetch/programmer-humour/fetch.js
+++ fetch/programmer-humour/fetch.js
@@ -1,13 +1,17 @@
 "use strict"
 const img = document.querySelector("img");
+const errorMessage = document.querySelector("#errorMessage")
 
 const fetchData = async () => {
-    const response = await fetch("https://xkcd.now.sh/?comic=latest");
-    const data = await response.json()
-    return data
+    try {
+        const response = await fetch("https://xkcd.now.sh/?comic=latest");
+        const data = await response.json()
+        return data
+    } catch (error) {
+        errorMessage.style.display = `contents`;
+        errorMessage.textContent = error;
+}
 }
 fetchData().then((data) => {
-    console.log(data.img)
     img.setAttribute("src", data.img)
-
 });
