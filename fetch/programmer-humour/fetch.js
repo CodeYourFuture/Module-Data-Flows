6c15a28612e7055344f8af496f71644420ed3c3d
Droid-An <andrei.filippov.aaf@gmail.com>
2025-04-15 14:44:15 +0100

    formatted documents

--- fetch/programmer-humour/fetch.js
+++ fetch/programmer-humour/fetch.js
@@ -1,17 +1,17 @@
-"use strict"
+"use strict";
 const img = document.querySelector("img");
 const errorMessage = document.querySelector("#errorMessage");
 
 const fetchData = async () => {
-    try {
-        const response = await fetch("https://xkcd.now.sh/?comic=latest");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        errorMessage.style.display = `contents`;
-        errorMessage.textContent = error;
-}
-}
+  try {
+    const response = await fetch("https://xkcd.now.sh/?comic=latest");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    errorMessage.style.display = `contents`;
+    errorMessage.textContent = error;
+  }
+};
 fetchData().then((data) => {
-    img.setAttribute("src", data.img);
+  img.setAttribute("src", data.img);
 });
