2b5bd0bea70cce6a2daafce1c289547bfa1e964f
katarzynakaz <l453r3@gmail.com>
2025-04-22 15:35:26 +0100

    programmer humor PR issues fixed

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -1,32 +1,30 @@
 async function fetchLatestComic() {
-    const endpoint = 'https://xkcd.now.sh/?comic=latest';
-    const forImg= document.getElementById('forImg');
-    const message = document.querySelector('.error');
-    
+  const endpoint = "https://xkcd.now.sh/?comic=latest";
+  const forImg = document.getElementById("forImg");
+  const message = document.querySelector(".message");
 
-    try {
-        const response = await fetch(endpoint);
+  try {
+    const response = await fetch(endpoint);
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log(data); 
-
-        const img = document.createElement('img');
-        img.src = data.img;
-        img.alt = data.alt;
-        
-        forImg.appendChild(img);
-    
-        message.textContent = '';
-
-    } catch (error) {
-        console.error('Error fetching the comic:', error);
-
-        errorMessage.textContent = ':( Please try again later.';
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+
+    const data = await response.json();
+    console.log(data);
+
+    const img = document.createElement("img");
+    img.src = data.img;
+    img.alt = data.alt;
+
+    forImg.appendChild(img);
+
+    message.textContent = "";
+  } catch (error) {
+    console.error("Error fetching the comic:", error);
+
+    message.textContent = ":( Please try again later.";
+  }
 }
 
 fetchLatestComic();
