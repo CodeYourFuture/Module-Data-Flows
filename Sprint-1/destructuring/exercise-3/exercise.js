725a54a05434ef443eb71a2055898fa3c25e2736
Nataliia74 <volkov.nataliia@gmail.com>
2025-07-22 21:22:19 +0100

    exercise-3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,20 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function getTotalOfOrder (order) {
+  let Total = 0;
+  console.log("QTY".padEnd(8), "ITEM".padEnd(24), "TOTAL");
+    order.forEach(({itemName, quantity, unitPricePence}) => {
+      const QTY = quantity;
+      const ITEM = itemName;
+      const TOTAL = ((unitPricePence * quantity)/100);
+      Total = Total + TOTAL;
+      console.log(`${String(QTY).padEnd(6)}   ${String(ITEM).padEnd(24)} ${(TOTAL.toFixed(2)).padStart(4)}`);
+    })
+  
+    console.log(`\nTotal:  ${Total.toFixed(2)}`)
+}
+
+
+getTotalOfOrder(order);
