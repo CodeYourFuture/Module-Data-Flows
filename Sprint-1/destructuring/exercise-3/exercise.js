3e204dc7810b410ef78c810539c47d11645eaf43
Aida Eslami <ayda.eslami@gmail.com>
2025-07-22 02:17:05 +0100

    Receipt

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,30 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+
+function printReceipt(order) {
+  let total = 0;
+  console.log("QTY  ITEM                 TOTAL");
+  order.forEach(record => {
+    const { itemName, quantity, unitPricePence } = record;
+    const lineTotal = quantity * unitPricePence;
+    total += lineTotal;
+
+    console.log(
+      String(quantity).padEnd(5) +
+      itemName.padEnd(20) +
+      convertPenceToPound(lineTotal).padStart(6)
+    );
+  });
+
+  console.log("\nTotal: " + convertPenceToPound(total));
+}
+
+function convertPenceToPound(pence) {
+  const pounds = Number(pence) / 100;
+  return pounds.toFixed(2);
+}
+
+printReceipt(order);
