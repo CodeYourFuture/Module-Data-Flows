7a7bba2e23fa320baee27b2cb4141b9dacfdedad
galyna-k <hkzlvs@gmail.com>
2025-07-23 12:14:08 +0100

    generate takeout receipt using reduce and destructuring

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,16 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+// Display the order summary
+console.log(`QTY     ITEM                TOTAL`);
+
+// Calculate the total for each item and the overall total
+let total = order.reduce((sum, { quantity, itemName, unitPricePence }) => {
+  let lineTotal = (quantity * unitPricePence) / 100;
+  console.log(`${quantity}       ${itemName.padEnd(20)}${lineTotal.toFixed(2)}`);
+  return sum + lineTotal;
+}, 0);
+
+// Display the overall total
+console.log(`\nTotal: ${total.toFixed(2)}`);
