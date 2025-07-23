fbe9aaf3ef9dadc08f7de37a387b35bdaf2835f8
Sara Tahir <saratahir34@gmail.com>
2025-07-23 17:16:42 +0100

    fixed my logic and rewrote the function to print out the result as expected.

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,12 +6,19 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
-let sum
-function receipt (order){
-  for({itemName,quantity,unitPricePence} of order) {
-    order.reduce((acc,curr)=> acc +curr,0)
-    console.log(`QTY ${quantity}  ITEM ${itemName} TOTAL ${unitPricePence}`)
+function receipt(order) {
+  let sum = 0;
+  console.log("QTY     ITEM                TOTAL"); //Prints a header for the table
+  for (let { itemName, quantity, unitPricePence } of order) {
+    let lineTotal = (quantity * unitPricePence) / 100;//Converts pence to pounds 
+    sum += lineTotal; //keeps a running total of all order cost.
 
+    console.log(
+      `${quantity.toString().padEnd(6)}${itemName.padEnd(22)}${lineTotal.toFixed(2)}`//.padEnd(n) adds spaces to the end of a string until it reaches n characters long
+    );
   }
+
+  console.log(`\nTotal: ${sum.toFixed(2)}`); // \n adds a blank line before console.log for better readibility.
 }
-receipt(order)
+
+receipt(order);
