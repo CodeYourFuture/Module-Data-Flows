155c903656154c338cd417305af02ec913603b12
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-07-24 13:21:29 +0100

    Updated the parameters in the function.

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -1,12 +1,12 @@
 const personOne = {
   name: "Popeye",
   age: 34,
-  favouriteFood: "Spinach",
+  favuoriteFood: "Spinach",
 };
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself(name, age, favouriteFood) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
