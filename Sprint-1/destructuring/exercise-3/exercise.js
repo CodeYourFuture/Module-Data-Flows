626f7b240b22bfe1bdcf9ff2f740a645e24de822
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-25 15:16:14 +0100

    writing a program that print receipt order, and total.

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,34 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+
+const quantityWidth = 9
+const itemWidth = 18
+
+const headQuantity = 'QTY'.padEnd(9)
+const headItem = 'ITEM'.padEnd(18)
+
+console.log(`${headQuantity}${headItem} TOTAL`)
+
+const isOrder =  (number) => typeof number.quantity==='number'
+order.forEach((item) => {
+
+const totalUnit = item.quantity*item.unitPricePence
+const orderNumber = Number(item.quantity)
+const nameItem = String(item.itemName)
+
+const orderNumberAlign = String(orderNumber).padEnd(9)
+const nameItemAlign= nameItem.padEnd(18)
+
+console.log(`${orderNumberAlign} ${nameItemAlign}${totalUnit}`)
+});
+
+
+
+const total = order.reduce((accumulator,item) =>{
+  itemTotal =item.quantity*item.unitPricePence
+  return accumulator + itemTotal
+},0)
+
+console.log(`Total: ${total}`)
