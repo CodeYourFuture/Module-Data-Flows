78abc5b8581ce85e0f192d6a58605cccbf3ade07
sedazam <sedazam@gmail.com>
2025-07-27 20:39:44 +0100

    Add order processing to calculate and display total cost

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,10 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY     ITEM                TOTAL");
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  let itemTotal = (quantity * unitPricePence) / 100;
+  totalCost += itemTotal;
+  console.log(`${String(quantity).padEnd(8)} ${itemName.padEnd(20)} ${itemTotal.toFixed(2)}`);
+});
