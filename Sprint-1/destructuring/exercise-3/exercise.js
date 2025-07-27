584495271576dffc4e48cd580f99854279993be8
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-27 23:54:17 +0100

    print formatted receipt using destructuring and running total

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,9 +6,12 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
-console.log("QTY", "       ", "ITEM", "        ", "TOTAL");
+console.log("QTY".padEnd(8), "ITEM".padEnd(18), "TOTAL".padStart(6));
+let billTotal = 0
 order.forEach(({ itemName, quantity, unitPricePence }) => {
-  const total = ((quantity * unitPricePence)/100).toFixed(2);
-  console.log("🚀 ~ total:", total)
-  
+  const itemTotal = ((quantity * unitPricePence)/100).toFixed(2);
+  billTotal += Number(itemTotal)
+  console.log(quantity.toString().padEnd(8), itemName.padEnd(18), itemTotal.padStart(5)); 
 });
+console.log("\n");
+console.log(`Total: ${billTotal.toFixed(2)}`);
