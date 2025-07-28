c2ddc6c21b44de45f46a5018fb019c890b09198d
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-07-28 20:39:51 +0200

    exercise-3 complete

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,19 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printHeading(){
+  console.log('QTY'.padEnd(7),'ITEM'.padEnd(19),'TOTAL');
+}
+
+function printOrder(order) {
+  const finalTotal = order.reduce((acc,{itemName, quantity, unitPricePence}) => {
+  const total = (unitPricePence * quantity) / 100;
+  console.log(`${quantity.toString().padEnd(8)}${itemName.padEnd(20)}${total.toFixed(2)}`);
+  return acc + total;
+},0)
+console.log( `\nTotal: ${finalTotal.toFixed(2)}`)
+}
+
+printHeading()
+printOrder(order)
