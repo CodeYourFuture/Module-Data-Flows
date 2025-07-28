9dbdd26703d165abb83d72c0fcc923960691e48c
halimatou-saddiyaa <halimatou.saddiyaa@gmail.com>
2025-07-28 14:27:51 +0100

    Updating the parameters in the introduceYourself function

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,27 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printOutOrder() {
+  console.log("QTY    ITEM                TOTAL");
+  let total = 0;
+
+  order.forEach(item => {
+    const { itemName, quantity, unitPricePence } = item;
+    const itemTotal = (unitPricePence * quantity) / 100;
+    total += itemTotal;
+
+    const qtyStr = quantity.toString().padEnd(7);
+    const itemStr = itemName.padEnd(20);
+    const totalStr = itemTotal.toFixed(2);
+
+    console.log(`${qtyStr}${itemStr}${totalStr}`);
+  });
+
+  console.log(`\nTotal: ${total.toFixed(2)}`);
+}
+
+printOutOrder();
+
+
+
