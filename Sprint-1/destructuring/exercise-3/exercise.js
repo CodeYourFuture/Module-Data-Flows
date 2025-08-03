c0d67add81eb00f9b7462e87680cbe5918e0647b
mirabellemorah <myrakuti@gmail.com>
2025-08-03 20:46:19 +0100

    excercise 3 done

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,19 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY     ITEM                TOTAL");
+
+let totalCost = 0;
+
+order.forEach((item) => {
+  let itemName = item.itemName;
+  let quantity = item.quantity;
+  let unitPricePence = item.unitPricePence;
+
+  const total = (quantity * unitPricePence) / 100;
+  totalCost += total;
+  console.log(`${quantity}       ${itemName.padEnd(20)} ${total.toFixed(2)}`);
+});
+
+console.log("\nTotal: " + totalCost.toFixed(2));
