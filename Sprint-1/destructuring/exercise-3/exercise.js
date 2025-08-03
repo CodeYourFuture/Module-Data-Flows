72e1ca28959f12743c597bd966b34d66c211e58e
3bdullah-saleh <abdullahedu0@gmail.com>
2025-08-03 16:51:57 +0100

    use array.reduce function instead of forEach function

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -7,11 +7,15 @@ let order = [
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
 console.log("QTY".padEnd(8), "ITEM".padEnd(18), "TOTAL".padStart(6));
-let billTotal = 0
-order.forEach(({ itemName, quantity, unitPricePence }) => {
-  const itemTotal = ((quantity * unitPricePence)/100).toFixed(2);
-  billTotal += Number(itemTotal)
-  console.log(quantity.toString().padEnd(8), itemName.padEnd(18), itemTotal.padStart(5)); 
-});
+const billTotal = order.reduce(
+  (total, { itemName, quantity, unitPricePence }) => {
+    const itemTotal = ((quantity * unitPricePence) / 100).toFixed(2);
+    console.log(
+      quantity.toString().padEnd(8),
+      itemName.padEnd(18),
+      itemTotal.padStart(5)
+    );
+    return total + Number(itemTotal);
+  }, 0);
 console.log("\n");
 console.log(`Total: ${billTotal.toFixed(2)}`);
