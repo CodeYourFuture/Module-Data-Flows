0d09dc18fff6ee48b913c14fdb4fb0a15d122aa1
mirabellemorah <myrakuti@gmail.com>
2025-08-09 13:35:32 +0100

    Fixed HTML, removed render that was double, changed names of variables to be easily understood, added trim to data

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -9,37 +9,37 @@ if (typeof window !== "undefined") {
 
 function populateStorage() {
   if (myLibrary.length == 0) {
-    let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
+    let book1 = new Book("Robison Crusoe", "Daniel Defoe", 252, true);
     let book2 = new Book(
       "The Old Man and the Sea",
       "Ernest Hemingway",
-      "127",
+      127,
       true
     );
     myLibrary.push(book1);
     myLibrary.push(book2);
-    render();
   }
 }
 
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const check = document.getElementById("check");
+const titleElement = document.getElementById("title");
+const authorElement = document.getElementById("author");
+const pagesElement = document.getElementById("pages");
+const checkButton = document.getElementById("check");
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
+
 function submit() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
+  if (!titleElement.value.trim() || !pagesElement.value.trim()) {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
+    let book = new Book(
+      titleElement.value,
+      authorElement.value,
+      parseInt(pagesElement.value, 10),
+      checkButton.checked
+    );
     myLibrary.push(book);
     render();
   }
