274dae0662e7f75e5d0c148186c1ed6a698a29b4
Sara Tahir <saratahir34@gmail.com>
2025-08-09 17:14:13 +0100

    used async function and await logic for fetching data.

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -6,23 +6,25 @@ img.addEventListener("click",() =>{
 },)
 comicLoading.innerHTML ="";
 comicLoading.appendChild(img)
-function loadComic(){
-fetch(`https://xkcd.now.sh/?comic=latest`)
-.then((response)=> {
-    if (!response.ok){
-        throw new Error ("Network is not responding")
+//using async await logic instead of fetch and then.
+async function loadComic() {
+  try {
+    const response = await fetch(`https://xkcd.now.sh/?comic=latest`);
+    if (!response.ok) {
+      throw new Error("Network is not responding");
     }
-    return response.json()
-})
-.then((data) => {
-    console.log(data)
-    img.src = data.img; 
-    img.alt = data.alt; 
-    img.title = data.title; 
-  })
-  .catch((error) => {
+
+    const data = await response.json();
+    console.log(data);
+
+    img.src = data.img;
+    img.alt = data.alt;
+    img.title = data.title;
+
+  } catch (error) {
     console.error("Failed to fetch comic:", error);
     comicLoading.textContent = "Oops! Failed to load comic.";
-  });
+  }
 }
-loadComic()
+
+loadComic();
