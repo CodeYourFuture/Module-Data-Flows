b25fa413ac0de28987c2dde2c43d1112651b5d85
Samiuk <lahlahhoussam80@gmail.com>
2025-08-13 18:53:54 +0100

    - Answer the questions in the Readme File. - Use object destructuring in introduceYourself parameter

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,9 +6,9 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
+// Use object destructuring in introduceYourself parameter
 function introduceYourself({ name, age, favouriteFood }) {
-  // Use the destructured variables in the console.log statement.
-  // Don't change anything else.
+
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
