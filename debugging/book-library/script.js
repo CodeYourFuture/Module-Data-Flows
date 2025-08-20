408cf69457e6bd2bbe440681e744ebe762e13d3b
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-20 18:55:53 +0100

    Refactor submit function to stop books from getting added if any input field is empty

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -28,12 +28,7 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
+  if (!title.value.trim() || !author.value.trim() || !pages.value.trim()) {
     alert("Please fill all fields!");
     return false;
   } else {
