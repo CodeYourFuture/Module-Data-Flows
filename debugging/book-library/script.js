dcd06c697f3d9d972370d4500516cfd5f3264ced
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-20 23:20:41 +0100

    Refactor submit function to use ternary statment.

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -27,19 +27,23 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
-  if (!title.value.trim() || !author.value.trim() || !pages.value.trim()) {
-    alert("Please fill all fields!");
-    return false;
-  } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
-    myLibrary.push(book);
-    render();
-    //clear user input data after storing the information
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    check.checked = false;
-  }
+  return !title.value.trim() || !author.value.trim() || !pages.value.trim()
+    ? (alert("Please fill all fields!"), false)
+    : function () {
+        let book = new Book(
+          title.value,
+          author.value,
+          pages.value,
+          check.checked
+        );
+        myLibrary.push(book);
+        render();
+        //clear user input data after storing the information
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        check.checked = false;
+      };
 }
 
 function Book(title, author, pages, check) {
