935addae90cfaccbb8bdc91e1505c80d6016b5ce
Samiuk <lahlahhoussam80@gmail.com>
2025-08-20 18:05:54 +0100

    - setup project with factorial.js and test file - implement factorial function using iterative approach

--- challenges/unit-testing/passing-tests/factorial/factorial.js
+++ challenges/unit-testing/passing-tests/factorial/factorial.js
@@ -1,13 +1,13 @@
-// int is an integer
-// a factorial is the product of all non-negative integers
-// less than or equal to the iniital number.
+function factorial(int) {
+  if (int === 0) return 1;
 
-// for example the factorial of 5 is 120
-// 120 = 1 * 2 * 3 * 4 * 5
+  let result = 1;
+  for (let i = 1; i <= int; i++) {
+    result *= i;
+  }
 
-// calculate and return the factorial of int
-// note: factorial of 0 is 1
-
-function factorial(int) {}
+  return result;
+}
 
 module.exports = factorial;
+
