4de83c5f70dc79c6c3b5f1aaf92847a5001930d0
Imran Mohamed <info@imranbmohamed.com>
2025-11-14 19:48:19 +0000

    Implement printReceipt function to display order details and total cost

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,22 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printReceipt(order){
+  let total = 0 , qtyWidthMax = 3, itemWidthMax = 4
+  const receiptLines = order.map(({itemName, quantity, unitPricePence}) => {
+    const itemTotalPounds = (quantity*unitPricePence/100);
+    qtyWidthMax = Math.max(qtyWidthMax, String(quantity).length);
+    itemWidthMax = Math.max(itemWidthMax, itemName.length);
+    return { itemName, quantity, itemTotalPounds };
+  })
+  console.log('QTY'.padEnd(qtyWidthMax+4), 'ITEM'.padEnd(itemWidthMax+3), 'TOTAL');
+  receiptLines.forEach(({itemName, quantity, itemTotalPounds}) => {
+    total += itemTotalPounds;
+    console.log(String(quantity).padEnd(qtyWidthMax+4), itemName.padEnd(itemWidthMax+3), `${itemTotalPounds.toFixed(2)}`);
+  })
+  console.log(`\nTotal: ${total.toFixed(2)}`);
+  
+}
+
+printReceipt(order);
