b39462dabd4f08ecab97aa1dbde2f39f7d4a41e1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-27 15:08:32 +0000

    got the price for each item in pounds and pence

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,12 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+order.map(({ itemName, quantity, unitPricePence }) => {
+  let totalPence = quantity * unitPricePence;
+  let pence = totalPence % 100;
+  let paddedPence = String(pence).padStart(2, "0");
+  let pounds = String(totalPence).substring(0, String(pence).length - 1);
+  let priceInPoundsAndPence=`${pounds}.${paddedPence}`
+  
+});
