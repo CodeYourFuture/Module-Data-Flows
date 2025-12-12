53ddf7a31d7e06ce2b7363f8b674768cabdc8264
Ammad <anosidium@hotmail.com>
2025-12-12 18:33:01 +0000

    Improve submit() function by setting the book author as author rather than title and improve the form validation to checks

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -28,16 +28,11 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
+  if (title.value == "" || author.value == "" || pages.value == "") {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, title.value, pages.value, check.checked);
+    let book = new Book(title.value, author.value, pages.value, check.checked);
     myLibrary.push(book);
     render();
   }
