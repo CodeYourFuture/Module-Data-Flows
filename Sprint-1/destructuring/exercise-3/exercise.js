720733e263627f34cf15170b2e9bc238099b1095
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-12-12 20:15:32 +0000

    Impelment receipt function using destructuring and same format

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,19 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+function receipt(arr){
+  let total=0;
+  const firstLine="QTY".padEnd(8)+"ITEM".padEnd(20)+"Total";
+  console.log(firstLine);
+
+
+arr.forEach(({itemName,quantity,unitPricePence})=> {
+  itemPrice=unitPricePence*quantity/100;
+  const line=String(quantity).padEnd(8)+itemName.padEnd(20)+itemPrice.toFixed(2);
+  console.log(line);
+  total+=itemPrice
+})
+console.log(`\nTotal:  ${total.toFixed(2)}`)
+}
+
+receipt(order);
