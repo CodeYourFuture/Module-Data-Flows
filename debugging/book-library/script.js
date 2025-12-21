9fcdbe72e6e313e5d00e8ee3d1b519905f81a0b3
asaniDev <asanidev9@gmail.com>
2025-12-21 08:21:21 +0000

    used the feedback improvement guide to check other errors i might have missed, and fixed those errors.

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -19,28 +19,41 @@ function populateStorage() {
   }
 }
 
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const check = document.getElementById("check");
+const titleElem = document.getElementById("title");
+const authorElem = document.getElementById("author");
+const pagesElem = document.getElementById("pages");
+const checkBox = document.getElementById("check");
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
-function submit() {
-  if (
-    title.value == "" ||
-    author.value == "" ||
-    pages.value == ""||
-    pages.value <= 0 
+export function submit() {
+  
+  if (!Number.isInteger(Number(pagesElem.value))){
+    alert("Pages must be an integer !");
+    return false;
+  }
+  else if (
+    titleElem.value.trim() == "" ||
+    authorElem.value.trim() == "" ||
+    pagesElem.value == ""||
+    pagesElem.value <= 0 
   ) {
     alert("Please fill all fields!");
     return false;
   } else {
-    let book = new Book(title.value, author.value, pages.value, check.checked);
+    let book = new Book(titleElem.value.toString().trim(), authorElem.value.trim(), Number(pagesElem.value), checkBox.checked);
     myLibrary.push(book);
     render();
+    titleElem.value = "";
+    authorElem.value = "";
+    pagesElem.value = "";
+    checkBox.checked = false;
   }
 }
+
+document.querySelector('#submitButton').addEventListener('click', () => {
+  submit();
+})
 
 function Book(title, author, pages, check) {
   this.title = title;
@@ -51,38 +64,37 @@ function Book(title, author, pages, check) {
 
 function render() {
   let table = document.getElementById("display");
-  let rowsNumber = table.rows.length;
-  //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- ){
-    table.deleteRow(n);
-  }
+  let tableBody = document.querySelector('tbody');
+  
+  tableBody.innerHTML = "";
+  
   //insert updated row and cells
   let length = myLibrary.length;
   for (let i = 0; i < length; i++) {
-    let row = table.insertRow(1);
+    let row = tableBody.insertRow(0);
     let titleCell = row.insertCell(0);
     let authorCell = row.insertCell(1);
     let pagesCell = row.insertCell(2);
     let wasReadCell = row.insertCell(3);
     let deleteCell = row.insertCell(4);
-    titleCell.innerHTML = myLibrary[i].title;
-    authorCell.innerHTML = myLibrary[i].author;
-    pagesCell.innerHTML = myLibrary[i].pages;
+    titleCell.innerText = myLibrary[i].title;
+    authorCell.innerText = myLibrary[i].author;
+    pagesCell.innerText = myLibrary[i].pages;
 
     //add and wait for action for read/unread button
-    let changeBut = document.createElement("button");
-    changeBut.id = i;
-    changeBut.className = "btn btn-success";
-    wasReadCell.appendChild(changeBut);
+    let changeButton = document.createElement("button");
+    changeButton.id = i;
+    changeButton.className = "btn btn-success";
+    wasReadCell.appendChild(changeButton);
     let readStatus = "";
     if (myLibrary[i].check === false) {
       readStatus = "No";
     } else {
       readStatus = "Yes";
     }
-    changeBut.innerText = readStatus;
+    changeButton.innerText = readStatus;
 
-    changeBut.addEventListener("click", function () {
+    changeButton.addEventListener("click", function () {
       myLibrary[i].check = !myLibrary[i].check;
       render();
     });
@@ -92,11 +104,14 @@ function render() {
     delButton.id = i + 5;
     deleteCell.appendChild(delButton);
     delButton.className = "btn btn-warning";
-    delButton.innerHTML = "Delete";
+    delButton.innerText = "Delete";
     delButton.addEventListener("click", function () {
-      alert(`You've deleted title: ${myLibrary[i].title}`);
+      const deletedTitle = myLibrary[i].title;
       myLibrary.splice(i, 1);
       render();
+      setTimeout(() => {
+        alert(`You've deleted title: ${deletedTitle}`);
+      }, 0)
     });
   }
 }
